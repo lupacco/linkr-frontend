14f0bde16376c1d4e81bb2092a406b632e8136b7
lupacco <pagottolc@gmail.com>
2023-03-07 21:15:36 -0300

    pages and componets structure

--- src/App.js
+++ src/App.js
@@ -1,3 +1,11 @@
+import {BrowserRouter, Route, Routes} from "react-router-dom"
+//Components
+import GlobalStyle from "./components/GlobalStyle";
+import Login from "./pages/Login"
+import SignUp from "./pages/SignUp"
+import Home from "./pages/Home"
+import User from "./pages/User"
+import Hashtag from "./pages/Hashtag"
 
 function App() {
   return (
