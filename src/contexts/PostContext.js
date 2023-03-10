c4d95fafe1bf80e67a1efbe00b3cbe2d4df5b65b
reiony <reiony.dev@gmail.com>
2023-03-10 17:54:09 -0300

    feat: Posts Implemented Static Page

---
+++ src/contexts/PostContext.js
@@ -0,0 +1,15 @@
+import { createContext, useState } from "react";
+
+const PostContext = createContext();
+
+export function PostProvider({ children }) {
+  const [posts, setPosts] = useState({});
+
+  return (
+    <PostContext.Provider value={[posts, setPosts]}>
+      {children}
+    </PostContext.Provider>
+  );
+}
+
+export default PostContext;
